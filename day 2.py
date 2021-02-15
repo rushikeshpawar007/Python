8eaeaaa43bad20dc31365e72255e81267e96780c
Rushikesh <78919818+rushikeshpawar007@users.noreply.github.com>
2021-02-15 19:47:02 +0530

    Day 2
    Day 2
    Day 02 of #100DaysOfCode challenge
    
    🔷 Python basics :-
    ✔ worked with Integers and floats, arithmetic Operations, Comparisons
    ✔ Source Code (GitHub) :-  
    
    #100DaysOfCode  #python

---
+++ day 2.py
@@ -0,0 +1,79 @@
+
+# Arithmetic Operators:
+# Addition:       3 + 2
+# Subtraction:    3 - 2
+# Multiplication: 3 * 2
+# Division:       3 / 2
+# Floor Division: 3 // 2
+# Exponent:       3 ** 2
+# Modulus:        3 % 2
+
+
+print( 3 + 2)
+print( 3 - 2)
+print( 3 * 2)
+print( 3 / 2)
+print( 3 // 2)
+print( 3 ** 2)
+print( 3 % 2)
+
+print(3 * 4 + 2)
+
+print (3 * (4 + 2))
+
+num = 1 
+
+num = num + 1
+
+print(num)
+
+num = 5 
+
+num *= 12
+
+print(num)
+
+print(abs(-3))
+
+
+print(round(3.75))
+
+print(round(3.75, 1)) # 1 is for the number of decimal places
+
+
+# Comparisons:
+# Equal:            3 == 2
+# Not Equal:        3 != 2
+# Greater Than:     3 > 2
+# Less Than:        3 < 2
+# Greater or Equal: 3 >= 2
+# Less or Equal:    3 <= 2
+# boolean values will be obtained
+
+
+num_1 = 2
+num_2 = 3
+
+print(num_1 == num_2)
+print(num_1 != num_2)
+print(num_1 < num_2)
+print(num_1 > num_2)
+print(num_1 >= num_2)
+print(num_1 <= num_2)
+
+
+#casting
+
+num_1 = '100'
+num_2 = '200'
+
+print(num_1 + num_2)
+
+num_1 = '100'
+num_2 = '200'
+
+num_1 = int(num_1)
+num_2 = int(num_2)
+
+print(num_1 + num_2)
+
