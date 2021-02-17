53c9a30818c49107a53de08abcf3e9132a093cda
Rushikesh <78919818+rushikeshpawar007@users.noreply.github.com>
2021-02-17 21:55:12 +0530

    Add files via upload
    ✔ Working with If else statement, Nested statement.
    ✔ Coding Exercise BMI Calculator v.2.0, Leap year< Pizza order.

--- day 3.py
+++ day 3.py
@@ -63,3 +63,95 @@ art_course = {'History', 'Maths', 'Art', 'Design'}
 print(cs_courses.intersection(art_course))
 print(cs_courses.difference(art_course))
 print(cs_courses.union(art_course))
+
+# If Else statement
+
+print('Welcome to the rollercoaster!')
+height = int(input('What is your height in cm? '))
+
+if height >= 120:
+    print('You can go for the ride!')
+    age = int(input('What is your age? '))
+    if age > 18:
+      print('you have to pay 12$')
+    elif age >= 12:
+      print('you have to pay 7$')
+    else:
+      print('you have to pay 5$')
+else:
+    print('Sorry, you need to grow taller to ride.')
+
+
+# BMI calculator v.20
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+bmi = round(weight / height**2, 2)
+
+print(bmi)
+
+if bmi < 18.5:
+    print(f'your bmi is {bmi}, you are underweight.')
+elif bmi < 25:
+    print(f'your bmi is {bmi}, you are normal weight.')
+elif bmi < 30:
+    print(f'your bmi is {bmi}, you are over weight.')
+elif bmi < 35:
+    print(f'your bmi is {bmi}, you are obse.')
+else:
+    print(f'your bmi is {bmi}, you are clinically obse.')
+
+
+# Leap year
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+if year % 4 != 0:
+  print('it is not a leap year')
+elif year % 100 == 0:
+  print('it is a leap year')
+elif year % 400 == 0:
+  print('it is a leap year')
+else:
+  print('it is not a leap year')
+
+
+#pizza order
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+bill = 0
+s = 15
+m = 20
+l = 25
+
+if size == s:
+  bill = s
+elif size == m:
+  bill = m
+else:
+  bill = l
+
+if add_pepperoni == 'Y':
+  if size == s:
+    bill += 2
+  else:
+      bill += 3
+
+if extra_cheese == "Y":
+  bill += 1
+  
+print(f"Your final bill is: ${bill}.")
+
+
