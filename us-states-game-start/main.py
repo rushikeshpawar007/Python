86f05490796758891a82fe183311c23ab74e6301
Rushikesh <78919818+rushikeshpawar007@users.noreply.github.com>
2021-03-12 23:45:10 +0530

    pandas project - guess the states

---
+++ us-states-game-start/main.py
@@ -0,0 +1,34 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{guessed_states}/50 states are correct", prompt="what's another state's "
+                                                                                            "name").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        print(missing_states)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states to learn")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_date = data[data.state == answer_state]
+        t.goto(int(state_date.x), int(state_date.y))
+        t.write(answer_state)
+
